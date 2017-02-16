b53f49036872d4687d309aa23a3667e1e5195436
FrustratedCirclem2 <frustratedcirclem2@163.com>
2017-02-16 22:10:55 +0800

    完善选择城市模块

--- LAGOU.COM/js/index_city.js
+++ LAGOU.COM/js/index_city.js
@@ -1,19 +1,28 @@
-(function() {
-	var changCity_Btn = document.getElementsByClassName("gw")[0];
-	var shade = document.getElementsByClassName("shade")[0];
-	var city_selectbox = document.getElementsByClassName("city_selectbox")[0];
-	var close_btn = document.getElementsByClassName("city_bar")[0].getElementsByTagName("img")[0];
-	
-	function shadeClear(){
-		shade.style.display = "none";
-		city_selectbox.style.display = "none";
-	}
-	
-	changCity_Btn.onclick = function (){
-		shade.style.display = "block";
-		city_selectbox.style.display = "block";
-	}
-	
-	shade.onclick = shadeClear;
-	close_btn.onclick = shadeClear;
-}) ();
+(function () {
+  var changCity_Btn = document.getElementsByClassName("gw")[0];
+  var shade = document.getElementsByClassName("shade")[0];
+  var city_selectbox = document.getElementsByClassName("city_selectbox")[0];
+  var close_btn = document.getElementsByClassName("city_bar")[0].getElementsByTagName("img")[0];
+  var currentCity = document.getElementById('currentCity');
+  function shadeClear() {
+    shade.style.display = "none";
+    city_selectbox.style.display = "none";
+  }
+
+  function changCity(event) {
+    var target = event.target;
+    if(target.tagName.toUpperCase() == 'A') {
+      var city = target.dataset.city;
+      currentCity.innerText = city;
+      shadeClear();
+    }
+  }
+
+  changCity_Btn.onclick = function () {
+    shade.style.display = "block";
+    city_selectbox.style.display = "block";
+  };
+  city_selectbox.onclick = changCity;
+  shade.onclick = shadeClear;
+  close_btn.onclick = shadeClear;
+})();
